b59eeb23fad48124a112425ff53a531589423ff2
Joseph Wilson <jwwilso5@asu.edu>
2019-05-03 05:37:12 +0000

    start of build file with ANT

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+	
+	public static void main(String[] args) {
+
+        Exercise testExercise = new Exercise(new Date(),"Hammer Curls", "Bitch strings", new Set[]{new Set(420, 69), new Set(69, 420)});
+        
+        System.out.println(testExercise.toString());
+	}
+	
+	
+}
---
+++ src/Set.java
@@ -0,0 +1,44 @@
+public class Set{
+	
+	private int amountOfReps;
+	private double weight;
+	
+	public Set(){
+		this.amountOfReps = 0;
+		this.weight = 0;
+	}
+	
+	public Set(int amountOfReps){
+		this.amountOfReps = amountOfReps;
+		this.weight = 0;
+	}
+
+	public Set(int amountOfReps, double weight){
+		this.amountOfReps = amountOfReps;
+		this.weight = weight;
+	}
+
+	public int getAmountOfReps(){
+		return amountOfReps;
+	}
+
+	public void setAmountOfReps(int amountOfReps){
+		this.amountOfReps = amountOfReps;
+	}
+
+	public double getWeight(){
+		return weight;
+	}
+
+	public void setWeight(double weight){
+		this.weight = weight;
+	}
+	
+	public String toString(){
+		StringBuilder ret = new StringBuilder();
+		ret.append("\t Reps: " + this.getAmountOfReps() + "\n");
+		ret.append("\t Weight: " + this.getWeight() + "\n\n");
+		return ret.toString();
+	}
+
+}
