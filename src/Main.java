59aef9628057ef9f190caacf33bc1b2319595f8d
Joseph Wilson <jwwilso5@asu.edu>
2019-05-07 19:08:46 +0000

    working Exercise to JSON formatted string

--- src/Main.java
+++ src/Main.java
@@ -12,10 +12,32 @@ public class Main {
         //Gson gson = new Gson();
         //String json = gson.toJson(testExercise);  
 
-        System.out.println(testExercise.toString());
-        System.out.println();
-        //System.out.println(json);
+        //System.out.println(testExercise.toString());
+        //System.out.println();
+        
+        String testExerciseJSON = exerciseToJSON(testExercise);
+        System.out.println(testExerciseJSON);
 	}
 	
+	public static String exerciseToJSON(Exercise exercise) {
+	    //private Date date;
+	    //private String exerciseName;
+	    //private String muscleCategory;
+	    //private Set[] sets;
+	    StringBuilder JSONstring = new StringBuilder();
+	    Set[] tempSets = exercise.getSets();
+	    
+	    JSONstring.append("{\n" + "\"date\"" + " : \"" + exercise.getDate() + "\",\n"
+	    + "\"exerciseName\"" + " : \"" + exercise.getExerciseName() + "\",\n"
+	    + "\"muscleCategory\"" + " : \"" + exercise.getMuscleCategory() + "\",\n");
+	    for (int i = 0; i < tempSets.length; i++) {
+	        int setCount = i + 1;
+	        JSONstring.append("\"Sets\"" + " : \"" + setCount + "\",\n"
+	        + "\"Reps\"" + " : \"" + tempSets[i].getAmountOfReps() + "\",\n"
+	        + "\"Weight\"" + " : \"" + tempSets[i].getWeight() + "\"\n");
+	    }
+	    JSONstring.append("}");
+	    return JSONstring.toString();
+	}
 	
 }
